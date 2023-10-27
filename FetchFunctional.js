04bedd8b5244d1a5c1e22278c13ced809b70a9a9
d-bhanu <142707687+d-bhanu@users.noreply.github.com>
2023-10-27 11:25:21 +0530

    Add files via upload

---
+++ FetchFunctional.js
@@ -0,0 +1,34 @@
+import{useEffect,useState} from "react";
+export const FetchFunctional=()=>{
+    const[data,setData]=useState([]);
+    const getData=()=>{
+        fetch("https://jsonplaceholder.typicode.com/posts/")
+        .then((response)=>response.json())
+        .then((result)=>setData(result))
+        .catch((err)=>console.log("Error occured",err));
+
+    };
+    useEffect(()=>{
+        getData();
+
+    },[]);
+    return(
+        <div>
+            <table border="1" cellpadding="10px"align="center">
+                <th>S.No</th>
+                <th>Posts</th>
+                {
+                    data.map((d)=>(
+                        <tr Key={d.id}>
+                            <td align="center">{d.id}</td>
+                            <td align="left">{d.title}</td>
+                        </tr>
+
+                    ))
+                }
+                    
+            </table>
+        </div>
+    );
+};
+export default FetchFunctional;
